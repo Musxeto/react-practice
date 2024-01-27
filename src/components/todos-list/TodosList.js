3c8b68675b22077c0b14cb66fcccb01d00d7c94b
Mus <138971833+Musxeto@users.noreply.github.com>
2024-01-27 11:11:53 +0500

    27 JAN TODOS LIST

--- src/components/todos-list/TodosList.js
+++ src/components/todos-list/TodosList.js
@@ -1,11 +1,48 @@
 import React from 'react'
-
+import { useState } from 'react'
 function TodosList() {
+  const [tasks, setTasks] = useState([]);
+  
+  const addTask = (newTask) => {
+    setTasks([...tasks, {description: newTask, completed: false}])
+  };
+  const completeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = true;
+    setTasks(updatedTasks);
+  };
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+  
   return (
     <div>
-      
+      <h2>Todos List</h2>
+    <form onSubmit = {(event) =>{
+      event.preventDefault();
+      addTask(event.target.description.value);
+      event.target.description.value = '';
+    }}>
+      <input type="text" name="description"/>
+      <button type="submit">Add Task</button>
+    </form>
+    <div>
+      <ul>
+        {tasks.map((task,index)=>(
+          <li key = {index}>
+            <input type='checkbox'
+            checked = {task.completed}
+            onChange={()=>completeTask(index)}></input>
+            <h3>{task.description}</h3>
+          <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
     </div>
   )
 }
 
-export default todos-list
+export default TodosList
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Counter from './components/Counter.js';
-
+import TodosList from './components/todos-list/TodosList.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Counter />
+    <TodosList/>
   </React.StrictMode>
 );
