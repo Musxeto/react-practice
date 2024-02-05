0eb2a790d12fbaba93a732720f5e49eaccb3145a
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-05 11:46:33 +0500

    Day 4

---
+++ src/components/Day 4/ToggleSwtich.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function ToggleSwitch({ initialState = false, onToggle }) {
+  const [isOn, setIsOn] = useState(initialState);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+    if (onToggle) {
+      onToggle(!isOn);
+    }
+  };
+
+  return (
+    <div>
+      <label>Toggle Switch</label>
+      <input type='checkbox' checked={isOn} onChange={handleToggle} />
+      <span className={isOn ? 'on' : 'off'}></span>
+    </div>
+  );
+}
+
+export default ToggleSwitch;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import Counter from './components/Counter/Counter.js';
 import TodosList from './components/todos-list/TodosList.js';
 import ParentComponent from './components/controlled-input/ParentComponent.js';
 import DataFetcher from './components/fetch-data/DataFetcher.js';
-import FormValid from './components/form-validation/formValid.js';
+import FormValid from './components/form-validation/FormValid.js'; // Fix the import
+import ToggleSwitch from './components/Day4/ToggleSwitch.js'; // Fix the import
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Counter />
-    <TodosList/>
+    <TodosList />
     <ParentComponent />
     <DataFetcher />
     <FormValid />
+    <ToggleSwitch />
   </React.StrictMode>
 );
