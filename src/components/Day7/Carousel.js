0024842d061c0edcfae7984abb8e65f7c39e81eb
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-08 12:44:21 +0500

    DAY 7

--- src/components/Day7/Carousel.js
+++ src/components/Day7/Carousel.js
@@ -1,12 +1,24 @@
 import React,{useState} from 'react'
 import './Carousel.css';
 
-function Carousel() {
-  return (
-    <div>
-      
+function Carousel( {images} ) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+    return (
+    <div className='carousel'>
+      <button onClick={handlePrev} className='carousel-button'>Prev</button>
+     <img className='carousel-image' src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} />
+      <button onClick={handleNext} className='carousel-button'>Next</button>
     </div>
   )
 }
 
-export default Carousel
+export default Carousel;
