adf613a81e3dbcece39cc7b0bb9b1ae149efa8cf
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-08 13:25:51 +0500

    Day 7 Carousel

--- src/components/Day7/Carousel.js
+++ src/components/Day7/Carousel.js
@@ -1,24 +1,32 @@
 import React,{useState} from 'react'
 import './Carousel.css';
 
-function Carousel( {images} ) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
+const Carousel = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const handlePrev = () => {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+  
+    const handleNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
     return (
-    <div className='carousel'>
-      <button onClick={handlePrev} className='carousel-button'>Prev</button>
-     <img className='carousel-image' src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} />
-      <button onClick={handleNext} className='carousel-button'>Next</button>
-    </div>
-  )
-}
-
-export default Carousel;
+      <div className="carousel">
+        <button onClick={handlePrev} className="carousel-button">
+        <i class="arrow left"></i>
+        </button>
+        <img
+          className={`carousel-image ${currentIndex !== 0 ? 'fade-out' : ''} carousel-next`}
+          src={images[currentIndex]}
+          alt={`Image ${currentIndex + 1}`}
+        />
+        <button onClick={handleNext} className="carousel-button">
+          <i class="arrow right"></i>
+        </button>
+      </div>
+    );
+  };
+  
+  export default Carousel;
