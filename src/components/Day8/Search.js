35309dacfd8ffff62ebd07c3189bad3efe5b643c
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-18 09:17:04 +0500

    Day 8

---
+++ src/components/Day8/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function Search({ titles }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredTitles, setFilteredTitles] = useState(titles);
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    const filtered = titles.filter(title => title.toLowerCase().includes(query.toLowerCase()));
+    setFilteredTitles(filtered);
+  };
+
+  return (
+    <div>
+      <input type="text" placeholder='Search here...' value={searchQuery} onChange={handleSearchChange} />
+      <ul>
+        {filteredTitles.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Search;
