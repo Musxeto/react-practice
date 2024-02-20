f713c2855615e275db1439c24b4b8375a349cac8
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-20 15:21:44 +0500

    Tabs cONTAINER

---
+++ src/components/Day10/tabbed-navigation/Tabs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TabList from './TabList'
+
+function Tabs({childeren}) {
+  return (
+    <div>
+      <TabList>{childeren}</TabList>
+    </div>
+  )
+}
+
+export default Tabs
