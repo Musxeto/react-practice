b27475b2a75a38b270e3155edfa04c28efe0b474
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-20 14:05:41 +0500

    Day 10

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import ProgressBar from './components/Day9/ProgressBar.js';
 import ExampleApp from './components/Day9/DynamicRendering/DynamicRenderer.js';
 import ColorPicker from './components/Day9/colour-picker/ColorPicker.js';
 
+
+
 function App() {
   const options = ['kENDRICK LAMAR', 'UZI', 'Carti', 'YE'];
   const images = ['https://wallpapercave.com/wp/wp8782757.jpg', 'https://wallpaperboat.com/wp-content/uploads/2021/04/19/75467/bleach-07.jpg', 'https://images.alphacoders.com/819/81993.jpg', 'https://s1.1zoom.me/b5050/144/332698-dikemoon_1920x1080.jpg', 'https://wallpaperset.com/w/full/2/a/5/172758.jpg'];
