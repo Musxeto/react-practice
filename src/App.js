e50604d44b530ff558e68844a3ef8c4c0181a6ce
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-29 12:43:41 +0500

    ye

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Day12/pages/Home";
+import BookList from "./components/Day12/pages/BookList";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">books</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/Day4/NavigationBar.js
+++ src/components/Day4/NavigationBar.js
@@ -1,15 +1,15 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 function NavigationBar() {
   return (
     <nav>
-        <ul>
-            <li href='#'>Home</li>
-            <li href='#'>About</li>
-            <li href='#'>Contact</li>
-        </ul>
+      <ul>
+        <li href="#">Home</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
